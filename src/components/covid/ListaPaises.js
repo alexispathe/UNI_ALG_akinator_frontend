3a7332d65f6eb2c008130f06f81cf3a88958f8ca
Alexis Pathe <alexispatheguillen@gmail.com>
2022-12-05 11:47:41 -0600

    Se agrega el spinner a los datos covid

--- src/components/covid/ListaPaises.js
+++ src/components/covid/ListaPaises.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react"
 import { ConfiguracionGrafica } from "./ConfiguracionGraficas";
+import { Spinner } from "../akinator/Spinner";
 import axios from 'axios';
 export const ListaPaises = () => {
-    const [pais, setPais] = useState('');
+    // const [pais, setPais] = useState('');
     const [DataCovid, setData] = useState([]);
     const [nombrePais, setNombrePais] = useState([]);
+    const [statusSpinner, setStatusSpinner] = useState(false);
     useEffect(() => {
         axios.get('https://api.covid19api.com/total/country/' + "mx").then(res => {
             setData(res.data.filter(data => data.Date >= "2022-11-01"));
@@ -12,25 +14,28 @@ export const ListaPaises = () => {
 
         axios.get('https://api.covid19api.com/countries').then(res => {
             setNombrePais(res.data);
+            setStatusSpinner(true);
         })
     }, []);
     const handleChange = (option) => {
+        setStatusSpinner(false);
         // setPais(option.target.value)
         // console.log(option.target.value)
         axios.get('https://api.covid19api.com/total/country/' + option.target.value).then(res => {
             setData(res.data.filter(data => data.Date >= "2022-11-01"));
+            setStatusSpinner(true);
             // console.log(DataCovid)
         })
 
     }
     return (
         <>
-            {DataCovid.length >= 1 ?
-                <div className="container">
+            {DataCovid.length >= 1?
+                <div className="container mt-5">
                     <form>
                         <div className="form-group">
                             <select className="form-select" onChange={(value) => handleChange(value)}>
-                                {/* <option value="mx" selected>México</option> */}
+                                <option value="mx" >México</option>
                                 {nombrePais.map(nombre => (
                                     // console.log(nombre.ISO2)
                                     <option value={nombre.ISO2} key={nombre.ISO2}>{nombre.Country}</option>
@@ -38,10 +43,11 @@ export const ListaPaises = () => {
                             </select>
                         </div>
                     </form>
-                    <ConfiguracionGrafica DataCovid={DataCovid}/>
+
+                    {statusSpinner? <ConfiguracionGrafica DataCovid={DataCovid}/>: <Spinner/>}
                 </div>
 
-                : "No hay datos"
+                : <Spinner/>
 
             }
 
