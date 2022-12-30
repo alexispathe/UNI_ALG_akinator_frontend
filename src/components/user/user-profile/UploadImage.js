495b996756dc9e9587f792ffdd4c8f97ec4a0179
Alexis Pathe <alexispatheguillen@gmail.com>
2022-12-29 18:47:35 -0600

    Prevenimos posibles errores a la hora de seleccionar una imagen de perfil

--- src/components/user/user-profile/UploadImage.js
+++ src/components/user/user-profile/UploadImage.js
@@ -4,43 +4,65 @@ import '../../../Styles/user/uploadImage.css'
 export const UploadImage = ({ setImages, images }) => {
     const [name, setName] = useState('');
     const [file, setFile] = useState();
-    const [pathImage, setPathImage] = useState("https://talently.tech/blog/wp-content/uploads/2022/02/%C2%BFQue-es-un-framework-en-programacion-scaled-1200x675.jpg");
+    const [pathImage, setPathImage] = useState("");
+    const [status, setStatus] = useState();
     const sendImage = (e) => {
         e.preventDefault();
-        UploadServices.sendImage(name, file)
-            .then((result) => {
-                console.log("El resultado es: ", result)
+        if (((file.type === "image/png" || file.type === "image/jpg" || file.type === "image/jpeg")) && (file.size <= 100000)) {
+            UploadServices.sendImage(name, file)
+                .then((req) => {
+                    console.log(req.data)
+                    req.data.code === 404 ? setStatus(false) : setStatus(true);
+                }
+                ).catch(err => console.log(err));
+        }else setStatus(false)
 
-            }
-            ).catch(err => console.log(err));
 
     }
     // Esta funcion nos permitira mostrar una preview de la imagen que estamos subiendo
     const onfileChange = e => {
+        // Validamos que efectivamente se mande algo en el input de file
         if (e.target.files && e.target.files.length > 0) {
-            const file = e.target.files[0];
-            if (file.type.includes("image")) {
-                const reader = new FileReader();
-                reader.readAsDataURL(file);
+            //Desestructuramos el arreglo para solo devolver el objeto con la informacion de la imagen
+            const [file] = e.target.files;
+            const reader = new FileReader();
 
+            if ((file.type.includes("image") && (file.type === "image/png" || file.type === "image/jpg" || file.type === "image/jpeg")) && (file.size <= 100000)) {
+                reader.readAsDataURL(file);
                 reader.onload = function load() {
                     setPathImage(reader.result)
                 }
-                setFile(file)
+                setFile(file) //Aqui guardamos la imagen para despues enviarla
+                setStatus('');
+                document.querySelector('.btn-submit').disabled = false; //Habilitamos el boton
+               
             } else {
-                console.log("There was an error")
+                //Borramos el documento por no cumplir el formato que estamos solicitando
+                document.querySelector('input[name=file]').value = "";
+                setFile('') //Borramos la imagen
+                setPathImage('') //Quitamos la imagen en caso que exista 
+                setStatus(false);
+                document.querySelector('.btn-submit').disabled = true; //Desabilitamos el boton
+                
+
             }
         }
     }
     return (
         <>
             <div className="">
-                <form className="">
+                <form className="" onSubmit={sendImage}>
                     <div className="input-file">
-                        <input type="file"  onChange={onfileChange}/>
-                        <img className="image-fluid img-thumbnail image" src={pathImage} alt="image"/>
-                        <input type="text" placeholder ="enter a name" className="name-picture mt-2" onChange={(e=> setName(e.target.value))}/>
-                        <button className="btn btn-outline-primary btn-lg btn-block" type="submit" onClick={sendImage}>Guardar imagen</button>
+                        <input type="file" onChange={onfileChange} accept="image/jpeg, image/png, image/jpg" name="file" required />
+                        {pathImage ?
+                            <div>
+                                <img className="image-fluid img-thumbnail image" src={pathImage} alt="image" />
+                                <input type="text" placeholder="enter a name" className="name-picture mt-2" onChange={(e => setName(e.target.value))} />
+                            </div>
+                            : ''}
+                        {status ? <div className="alert alert-success text-center">Imagen actualizada correctamente..</div> : ''}
+                        {status === false ? <div className="text-center alert alert-danger">Selecciona una imagen valida...</div> : ''}
+                        <input type="submit" className="btn btn-primary btn-submit" value="Guardar imagen"   disabled/>
                     </div>
                 </form>
             </div>
